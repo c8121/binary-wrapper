c2698ca5e8bed7158a028a1e501fa2fb837d3cc8
Christian Hau <ch.hau@gefro.de>
2025-02-11 15:33:36 +0100

    Cleanup / Comments

--- BinaryWrapper/src/main/java/de/c8121/binarywrapper/Rsync.java
+++ BinaryWrapper/src/main/java/de/c8121/binarywrapper/Rsync.java
@@ -14,26 +14,41 @@ public class Rsync extends AbstractWrapper {
         super(LINUX_BINARY_NAME, WINDOWS_BINARY_NAME);
     }
 
+    /**
+     * <code>--version</code>
+     */
     public Rsync version() {
         this.addCommandOption("--version");
         return this;
     }
 
+    /**
+     * <code>-v</code>
+     */
     public Rsync verbose() {
         this.addCommandOption("-v");
         return this;
     }
 
+    /**
+     * <code>-a</code>
+     */
     public Rsync archive() {
         this.addCommandOption("-a");
         return this;
     }
 
+    /**
+     * <code>-r</code>
+     */
     public Rsync recursive() {
         this.addCommandOption("-r");
         return this;
     }
 
+    /**
+     * <code>--delete</code>
+     */
     public Rsync delete() {
         this.addCommandOption("--delete");
         return this;
@@ -51,6 +66,10 @@ public class Rsync extends AbstractWrapper {
         return this;
     }
 
+    /**
+     * Configures rsync to use ssh with sshpass (<code>-e "sshpass ... ssh.exe"</code>)
+     * Please note the "SECURITY CONSIDERATIONS" in <code>man sshpass</code>
+     */
     public Rsync useSshPass(char[] password) throws IOException {
         this.addCommandOption("-e",
                 new Ssh()
@@ -60,11 +79,18 @@ public class Rsync extends AbstractWrapper {
         return this;
     }
 
+
+    /**
+     * Path to local source directory or file
+     */
     public Rsync src(String src) {
         this.addCommandArgs(BINARY_PROVIDER.parsePathName(src));
         return this;
     }
 
+    /**
+     * Destination path or url like <code>[user@host:]path</code>
+     */
     public Rsync dest(String dest) {
         this.addCommandArgs(BINARY_PROVIDER.parsePathName(dest));
         return this;
